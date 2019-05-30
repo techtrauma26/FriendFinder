3d43357fe21e5b53c471023f069bded97061e43d
Vyjoo Frank <vlakshm26@gmail.com>
2019-05-30 19:16:05 -0400

    updated static

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 var public = require("path").join(__dirname,"app/public");
-app.use(express.static('public'));
+app.use(express.static(public));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
